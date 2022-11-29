91acc98bce9e8da4e308c1f9c1665b155501160e
GeorgeSwift16 <George.Swift@nology.io>
2022-11-29 18:10:22 +0000

    text change done, step done

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 /*                                  Global variables            */
-
-let answerString = "";
-let currentWordString = "";
-let isGameActive = true;
-// active output display box variables
-let activeOutputBoxIndex = 3;
-let activeOutputDisplayBox;
-
+let activeOutputBoxIndex = 0;
+// Active output box
 /*                                  DOM Variables               */
 //input variables
 //  Input Buttons
@@ -18,52 +12,38 @@ const allLetterInputButtons = document.querySelectorAll(".button-letter");
 const enterInputButton = document.getElementById("ENTER");
 const clearInputButton = document.getElementById("CLEAR");
 const resetInputButton = document.getElementById("RESET");
-console.log(resetInputButton);
 // Output variables
 //    All output boxes
 const outputDisplayBoxes = document.querySelectorAll(
   ".output-grid__display-box"
 );
-//  console.log();
-/*                                    Functions                               */
-
-// Functions for Clear input
-
-const handleClearInput = () => {
-  // check for clear edge cases - is the game active?
-  if (isGameActive && canWeGoBack) {
-    console.log("can go back");
-  } else {
-    console.log("cant go back");
-  }
+const handleLetterInput = (event) => {
+  outputDisplayBoxes[activeOutputBoxIndex].innerHTML = event.target.value;
 };
 
-//
-
-/*                Utility Functions                       */
 // function to change active square step
 const handleActiveOutputBox = (event) => {
-  let canWeGoBack = activeOutputBoxIndex % 5;
-  let activeOutputBoxIncrement = activeOutputBoxIndex;
-  if (event.target.value === "ENTER") {
-    activeOutputBoxIncrement += 1;
-  } else if (event.target.value === "CLEAR" && canWeGoBack != 0) {
-    activeOutputBoxIncrement -= 1;
-  } else if (event.target.value === "RESET") {
-    activeOutputBoxIncrement = 0;
+  // if (event.target.value ==)
+  changeActiveOutput(event.target.value);
+};
+
+const changeActiveOutput = (input) => {
+  if (input === "ENTER") {
+    activeOutputBoxIndex += 1;
+  } else if (input === "CLEAR" && activeOutputBoxIndex != 0) {
+    activeOutputBoxIndex -= 1;
+  } else if (input === "RESET") {
+    activeOutputBoxIndex = 0;
   }
-  activeOutputBoxIndex = activeOutputBoxIncrement;
-  activeOutputDisplayBox = document.querySelectorAll(
-    ".output-grid__display-box"
-  )[activeOutputBoxIndex];
+  outputDisplayBoxes[activeOutputBoxIndex];
 };
 
 /*                        Event listeners                   */
 //    all letter input buttons
-// for (let index = 0; index < allLetterInputButtons.length; index++) {
-//   allLetterInputButtons[index].addEventListener("click", handleLetterInput);
-// }
+for (let index = 0; index < allLetterInputButtons.length; index++) {
+  allLetterInputButtons[index].addEventListener("click", handleLetterInput);
+}
 //    specific operator input buttons - Enter, Clear, Reset
 enterInputButton.addEventListener("click", handleActiveOutputBox);
-clearInputButton.addEventListener("click", handleClearInput);
+clearInputButton.addEventListener("click", handleActiveOutputBox);
 resetInputButton.addEventListener("click", handleActiveOutputBox);
