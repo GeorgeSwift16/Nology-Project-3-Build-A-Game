328d49d1d60d0c8be4f7eafcec6893338434ad57
GeorgeSwift16 <George.Swift@nology.io>
2022-12-01 09:59:00 +0000

    refactored answer check fun before inputting it

--- checkingstuff.js
+++ checkingstuff.js
@@ -44,35 +44,37 @@ let answerArray = ["L", "B", "L", "L", "P"];
 // };
 // NOTES on what on earth im doing below - WE use the bottom function to check if an answer is correct, almost correct or not. We then pass this to the function below which goes across the array, and removes an element as it comes across it to replace it with Hi, this prevents the error in index passed due to 2 of the same elements. I will then use this to pass the function to change the display box colours. So to do in the morning -> replacing console.log(answercheckArr.indexOf(element)) with call color func with the same. the color func will then use activeoutputboxindex. then do axtiveoutputbox index - 4 + (the index passed) then colour the OutputDisplayBoxes array at this calculated numbers index!
 // Also need to change the else statement in the function below to push the letters that are incorrect to a new array called characters not in answer then have this function call one to apply the grey colour to any outputdisplybox with innertext = a char in the array.
-const updateGameColors = () => {
-  for (let index = 0; index < answerCheckArr.length; index++) {
-    let element = answerCheckArr[index];
-    if (element === "correct") {
-      console.log(answerCheckArr.indexOf(element));
-      answerCheckArr.splice(index, 1, "hi");
-    }
-    if (element === "almost") {
-      //   console.log(answerCheckArr.indexOf(element));
-    } else {
-      //   console.log(answerCheckArr.indexOf(element));
-    }
-  }
-  console.log(answerCheckArr);
-};
+// const updateGameColors = () => {
+//   for (let index = 0; index < answerCheckArr.length; index++) {
+//     let element = answerCheckArr[index];
+//     if (element === "match") {
+//       console.log(answerCheckArr.indexOf(element));
+//       answerCheckArr.splice(index, 1, "hi");
+//     }
+//     if (element === "almost") {
+//       //   console.log(answerCheckArr.indexOf(element));
+//     } else {
+//       //   console.log(answerCheckArr.indexOf(element));
+//     }
+//   }
+//   console.log(answerCheckArr);
+// };
 const checkCorrectCharCorrectIndex = (inputArr, answerArr) => {
   for (let index = 0; index < inputArr.length; index++) {
     let element = inputArr[index];
-    let isMatch = element === answerArr[index];
+    const isMatch = element === answerArr[index];
+    const isAlmostMatch = answerArr.includes(element) && !isMatch;
     if (isMatch) {
-      answerCheckArr.push("correct");
-    } else if (answerArr.includes(element) && !isMatch) {
+      answerCheckArr.push("match");
+    } else if (isAlmostMatch) {
       answerCheckArr.push("almost");
     } else {
       answerCheckArr.push(element);
     }
   }
-  updateGameColors();
+  // updateGameColors();
 };
 checkCorrectCharCorrectIndex(rowInputHtmlArray, answerArray);
 console.log(answerCheckArr);
-console.log(answerCheckArr[1]);
+
+// console.log(answerCheckArr[1]);
